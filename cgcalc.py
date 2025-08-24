b32aa562bb37d2cf09f9054388e92182714bdfe3
jbenson <jackmartinbenson@gmail.com>
2025-08-24 13:22:09 +1000

    cleaned things up and added comments

--- cgcalc.py
+++ cgcalc.py
@@ -5,6 +5,10 @@ import argparse
 from collections import deque
 
 class Trade:
+    """ 
+    As a class this represents a single trade, essentially a row in the csv.
+    This class is immutable after construction.
+    """
     __slots__ = ("stockType_", "units_", "isBuy_", "value_", "_frozen")
 
     def __init__(self, stockType:str, units:float, isBuy:bool, value:float):
@@ -23,69 +27,72 @@ class Trade:
         side = "Buy" if self.isBuy_ else "Sell"
         return f"Trade(stockType={self.stockType_}, units={self.units_}, side={side}, value={self.value_})"
 
-def find_col(key:str, heading:list[str]) -> int|None:
+def find_col(key:str, heading:list[str]) -> int:
+    """ Finds the column associated with a given keyword.
+    
+    Keyword Arguments:
+        key -- a text keyword in a column
+        heading -- the row being analyzed (meant for the heading of the csv)
+
+    Returns:
+        index:int -- the index of the column
+    """
     for index, x in enumerate(heading):
         if key in x:
             return index
+    raise ValueError(f"Heading '{key}' not found in CSV")
 
 def lex_csv(filename:str) -> list[Trade]:
-    if None == filename:
-        print("Failed to read file, filename = None")
-        return [] 
+    """Turns the csv into a list of 'Trade' objects.
     
-    try:
-        with open(filename, newline='') as csvfile:
-            reader = csv.reader(csvfile, delimiter=',')
-            
-            heading = next(reader)
-            stockSymbolCol = find_col("Symbol", heading)
-            totalValueCol = find_col("Total Value", heading)
-            sideCol = find_col("Side" ,heading)
-            unitsCol = find_col("Units", heading)
+    Keyword Arguments:
+        filename -- the name of the csv file
 
-            if stockSymbolCol is None:
-                print("Failed to find stockSymbolCol")
-                return []
-            elif totalValueCol is None:
-                print("Failed to find totalValueCol")
-                return []
-            elif sideCol is None:
-                print("Failed to find sideCol")
-                return []
-            elif unitsCol is None:
-                print("Failed to find unitsCol")
-                return []
-            
-            tradeList:list[Trade] = []
-            for row in reader:
-                isBuy:bool|None = None
-                value:float = float(row[totalValueCol])
-                symbol:str = row[stockSymbolCol]
-                units:float = abs(float(row[unitsCol]))
+    Returns:
+        trades:list[Trade]  -- A list of trade objects representing
+            each column of the csv.
 
-                if "Buy" in row[sideCol]:
-                    isBuy = True
-                elif "Sell" in row[sideCol]:
-                    isBuy = False
-                else:
-                    print("error finding side")
-                    return []
+    Exceptions:
+        FileNotFoundError
+        StopIteration
+        ValueError
+    """
+    with open(filename, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        
+        heading = next(reader)
+        stockSymbolCol = find_col("Symbol", heading)
+        totalValueCol = find_col("Total Value", heading)
+        sideCol = find_col("Side" ,heading)
+        unitsCol = find_col("Units", heading)
+        
+        tradeList:list[Trade] = []
+        for row in reader:
+            isBuy:bool|None = None
+            value:float = float(row[totalValueCol])
+            symbol:str = row[stockSymbolCol]
+            units:float = abs(float(row[unitsCol]))
 
-                tradeList.append(Trade(symbol, units, isBuy, value))
+            if "Buy" in row[sideCol]:
+                isBuy = True
+            elif "Sell" in row[sideCol]:
+                isBuy = False
+            else:
+                raise ValueError("Failed to parse 'Side'")
 
-            return tradeList
+            tradeList.append(Trade(symbol, units, isBuy, value))
 
-
-    except FileNotFoundError:
-        print("Failed to read file, file not found")
-    except StopIteration:
-        print("Failed to read file, file empty")
-    except ValueError:
-        print("Failed to parse data from csv field")
-
-    return []
+        return tradeList
 
 def calculate_capital_gains(trades:list[Trade]) -> tuple[float, float]:
+    """Tax a list of 'Trade' objects and calculates net gains and total gains.
+    
+    Keyword Arguments:
+        trades:list[Trade] -- a list of trade objects, ideally from lex_csv()
+
+    Returns:
+        net_gains:float, total_gains:float -- the net, then total gains
+    """
     # stock -> deque of (units, price per unit)
     holdings: dict[str, deque[tuple[float,float]]] = {}
     total_gains = 0.0
@@ -129,15 +136,13 @@ if __name__ == "__main__":
     argparser = argparse.ArgumentParser(
         prog="Stake - Captial Gains Calculator",
         description="calculates your capital gains based on a stake tax csv")
-
     argparser.add_argument('filename')
-
     args = argparser.parse_args()
 
-    net, total = calculate_capital_gains(lex_csv(args.filename))
-
-    print("Net Capital Gains is: ", net)
-    print("Total Capital Gains is: ", total)
-
-
+    try:
+        net, total = calculate_capital_gains(lex_csv(args.filename))
+        print("Net Capital Gains is: ", net)
+        print("Total Capital Gains is: ", total)
+    except (FileNotFoundError, StopIteration, ValueError) as e:
+        print(e)
 
